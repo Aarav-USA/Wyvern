09d4561425ae184e4c99ee3bff406ec6f2d691e7
XxMidasTouchxX <80801287+XxMidasTouchxX@users.noreply.github.com>
2021-05-27 00:53:23 -0400

    Update status.py

--- status.py
+++ status.py
@@ -1,3 +1,5 @@
+import discord
+from discord.ext import commands
 @client.event
 async def on_ready():
 await client.change_presence(status=discord.Status.idle, activity=discord. Game('for @HoneyComb'))
