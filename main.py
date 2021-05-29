9b81fcf7dfdcf6e2a4509ed7fef1ebec353378d9
Midas' Touch <80801287+XxMidasTouchxX@users.noreply.github.com>
2021-05-29 01:01:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ bot.load_extension('info')
 bot.load_extension('moderation')
 bot.load_extension('ticket.ticket')
 bot.load_extension('reaction')
+bot.load_extension('fun')
 
 @bot.event
 async def on_ready() -> None:
