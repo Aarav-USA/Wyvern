f1f8dc990fcfb68209297c7b95b5d9d0e6efe995
Asger <Asgerhoj@gmail.com>
2021-05-30 19:09:49 -0700

    implement kick command

--- moderation.py
+++ moderation.py
@@ -23,10 +23,22 @@ class Moderation(commands.Cog):
         await ctx.guild.ban(user, reason=reason)
         await ctx.channel.send(f'{user} is banned!')
 
-    @commands.command(help='Kicks a user')
-    async def kick(self, ctx: commands.Context, user: discord.User) -> None:
-        await ctx.send('Not Implemented')
-        raise NotImplementedError()
+    #kicks a user with a reason
+    @commands.command()
+    @commands.has_any_role('Administrators','Moderators,','Mods','Admins','Admin','Mod')
+    async def kick(self, ctx: commands.Context, user: discord.User, reason: str) -> None:
+        if user is None or user == ctx.message.author:
+            await ctx.channel.send('You cannot kick yourself!')
+            return
+        if reason is None:
+            reason = 'We have decided to kick you out of our server!'
+        if ctx.guild is None:
+            await ctx.send('You can only use this command in a server.')
+            return
+        message = f'You have been kicked from {ctx.guild.name} Reason: {reason}'
+        await user.send(message)
+        await ctx.guild.kick(user, reason=reason)
+        await ctx.channel.send(f'{user} has been kicked!')
 
     @commands.command(help='Mutes a user')
     async def mute(self, ctx: commands.Context, user: discord.User) -> None:
