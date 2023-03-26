b148a4c6439b77fa767191c987b638ac392f5e66
Presnap <80801287+Aarav-USA@users.noreply.github.com>
2023-03-26 19:55:29 -0400

    update main.py to async

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import discord
 from discord.ext import commands
 
 import configparser
+import asyncio
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -19,25 +20,71 @@ else:
 # Mypy doesn't understand an implicit setattr.
 setattr(bot, 'config', config)
 
-bot.load_extension('info')
-bot.load_extension('moderation')
-bot.load_extension('ticket.ticket')
-bot.load_extension('reaction')
-bot.load_extension('suicide_prevention.suicide_prevention')
-bot.load_extension('fun')
-bot.load_extension('welcome')
+cogs = [
+    'afk',
+    'altraider',
+    'auditlog',
+    'automod',
+    'boostperk',
+    'bulletin',
+    'captcha',
+    'channels',
+    'courier',
+    'customcommand',
+    'embed',
+    'emoji',
+    'entertainment',
+    'form',
+    'fun',
+    'giveaway',
+    'help',
+    'information',
+    'invitetrack',
+    'level',
+    'moderation',
+    'moneygame',
+    'music',
+    'platformalert',
+    'profile',
+    'reaction',
+    'reactionrole',
+    'serverlayout',
+    'serverstat',
+    'tempchannel',
+    'utility',
+    'voicemail',
+    'welcomegoodbye',
+    'wordrace'
+    ]
+
+name = 'Wyvern: '
+async def load_all_cogs():
+    for extension in cogs:
+        try:
+            async with bot:
+                await bot.load_extension("cogs." + extension)
+                print(name + f'Loaded {extension} cog.')
+        except Exception:
+            print(name + f'Error loading {extension} cog: {str(Exception)}')
 
 @bot.event
 async def on_ready() -> None:
     print("The bot is now online")
-    # Setting 'Watching status
+    # Setting 'Watching' status
     await bot.change_presence(activity=discord.Activity(
         type=discord.ActivityType.watching, name='for @Wyvern'))
-    print("----------------------------------------------")
+    print("-" * 47)
 
-if __name__ == '__main__':
-    bot_token = config['auth']['BotToken']
-    if bot_token == 'REDACTED':
-        print('Please add your bot token to the auth section in config.ini')
-    else:
-        bot.run(bot_token)
+async def main():
+    if __name__ == '__main__':
+        try:
+            await load_all_cogs()
+            await bot.start(config['auth']['BotToken'])
+        except KeyboardInterrupt:
+            print(name + "Keyboard interrupt, closing application.")
+        except Exception as e:
+            print(name + f"Error while running: '{e}'")
+        finally:
+            await bot.close()
+
+asyncio.run(main())
