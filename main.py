8bb63bceb34fccd59e8675d78bf7c3011fa89b23
Aarav USA <aaravbatra@duck.com>
2023-04-05 17:04:18 -0400

    change to single quote style and error handling

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ import asyncio
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-bot_mention = "@Wyvern"
+bot_mention = '@Wyvern'
+name = 'Wyvern:'
 if config['DEFAULT'].getboolean('AllowMentionPrefix'):
     bot = commands.Bot(discord.ext.commands.when_mentioned_or(*bot_mention),
         intents=discord.Intents.all(), case_insensitive=True)
+    print(f'{name} Mention prefix is allowed.')
 else:
     bot = commands.Bot(bot_mention,
         intents=discord.Intents.all(), case_insensitive=True)
+    print(f'{name} Mention prefix is not allowed.')
 
 # Mypy doesn't understand an implicit setattr.
 setattr(bot, 'config', config)
@@ -54,18 +57,17 @@ cogs = [
     # 'voicemail',
     ]
 
-name = 'Wyvern:'
 async def load_all_cogs():
     for extension in cogs:
         try:
-            await bot.load_extension("cogs." + extension)
+            await bot.load_extension('cogs.' + extension)
             print(f'{name} Loaded {extension} cog.')
         except Exception as e:
             print(f'{name} Error loading {extension} cog: {str(e)}')
 
 @bot.event
 async def on_ready() -> None:
-    print(f"{name} Successfully connected to Discord.")
+    print(f'{name} Successfully connected to Discord.')
     # Setting 'Watching' status
     await bot.change_presence(activity=discord.Activity(
         type=discord.ActivityType.watching, name=f'for @Wyvern'))
@@ -75,7 +77,7 @@ async def main():
     try:
         await bot.start(config['auth']['BotToken'])
     except KeyboardInterrupt:
-        print(f"{name} Keyboard interrupted, disconnecting...")
+        print(f'{name} Keyboard interrupted, disconnecting...')
     except Exception as e:
         print(f"{name} Error while running: '{e}'")
     finally:
