4accb8ceea912ff0597ee9b919a970a4f2108c38
Aarav USA <aaravbatra@duck.com>
2023-04-07 01:09:02 -0400

    cleaned up main.py, and updated moderation.py

--- cogs/moderation/moderation.py
+++ cogs/moderation/moderation.py
@@ -1,52 +1,64 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
+from discord.app_commands import Choice
 
-class Moderation(commands.Cog):
+class moderation(commands.Cog):
 
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-    #bans a user with a reason
-    @commands.command()
-    @commands.has_any_role('Administrators','Moderators,','Mods','Admins','Admin','Mod')
-    async def ban(self, ctx: commands.Context, user: discord.User, *,
-            reason: str) -> None:
-        if user is None or user == ctx.message.author:
-            await ctx.channel.send('You cannot ban yourself!')
-            return
-        if reason is None:
-            reason = 'We have decided you ban you out of our server!'
-        if ctx.guild is None:
-            await ctx.send('You can only use this command in a server.')
+    # bans a user with a reason
+    @commands.has_permissions(ban_members=True)
+    @app_commands.command(name="ban", description="Ban a member from your guild")
+    @app_commands.describe(
+        member = "Member to ban",
+        reason = "Reason for ban",
+        # duration = "Amount of time to ban member",
+        # preserve = "Preserve member messages",
+        # appeal = "Appeal the ban",
+    )
+
+    # @app_commands.choices(preserve = [
+    #     Choice(name = "True", value = True),
+    #     Choice(name = "False", value = False)
+    # ])
+    # @app_commands.choices(appeal = [
+    #     Choice(name = "True", value = True),
+    #     Choice(name = "False", value = False)
+    # ])
+    async def ban(self, ctx: commands.Context, member: discord.Member, *,
+            reason: str = f"The moderators have decided to ban you from this guild.") -> None:
+        if member is None or member == ctx.author:
+            await ctx.send('You cannot ban yourself!')
             return
         message = f'You have been banned from {ctx.guild.name} Reason: {reason}'
-        await user.send(message)
-        await ctx.guild.ban(user, reason=reason)
-        await ctx.channel.send(f'{user} is banned!')
+        await member.send(message)
+        await ctx.guild.ban(member, reason=reason)
+        await ctx.send(f'{member} is banned!')
 
-    #kicks a user with a reason
-    @commands.command()
-    @commands.has_any_role('Administrators','Moderators,','Mods','Admins','Admin','Mod')
-    async def kick(self, ctx: commands.Context, user: discord.User, *,
-            reason: str) -> None:
-        if user is None or user == ctx.message.author:
-            await ctx.channel.send('You cannot kick yourself!')
-            return
-        if reason is None:
-            reason = 'We have decided to kick you out of our server!'
-        if ctx.guild is None:
-            await ctx.send('You can only use this command in a server.')
-            return
-        message = f'You have been kicked from {ctx.guild.name} Reason: {reason}'
-        await user.send(message)
-        await ctx.guild.kick(user, reason=reason)
-        await ctx.channel.send(f'{user} has been kicked!')
+#     # kicks a user with a reason
+#     @commands.command()
+#     @commands.has_any_role('Administrators','Moderators,','Mods','Admins','Admin','Mod')
+#     async def kick(self, ctx: commands.Context, user: discord.User, *,
+#             reason: str) -> None:
+#         if user is None or user == ctx.message.author:
+#             await ctx.channel.send('You cannot kick yourself!')
+#             return
+#         if reason is None:
+#             reason = 'We have decided to kick you out of our server!'
+#         if ctx.guild is None:
+#             await ctx.send('You can only use this command in a server.')
+#             return
+#         message = f'You have been kicked from {ctx.guild.name} Reason: {reason}'
+#         await user.send(message)
+#         await ctx.guild.kick(user, reason=reason)
+#         await ctx.channel.send(f'{user} has been kicked!')
 
-    @commands.command(help='Mutes a user')
-    async def mute(self, ctx: commands.Context, user: discord.User) -> None:
-        await ctx.send('Not Implemented')
-        raise NotImplementedError()
+#     @commands.command(help='Mutes a user')
+#     async def mute(self, ctx: commands.Context, user: discord.User) -> None:
+#         await ctx.send('Not Implemented')
+#         raise NotImplementedError()
 
-def setup(bot: commands.Bot) -> None:
-    print('Loading moderation extension...')
-    bot.add_cog(Moderation(bot))
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(moderation(bot))
--- main.py
+++ main.py
@@ -9,82 +9,55 @@ import asyncio
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-if config['DEFAULT'].getboolean('AllowMentionPrefix'):
-    command_prefix = command_prefix=commands.when_mentioned
+# prefix required for slash commands to initialize
+command_prefix = commands.when_mentioned
 
+if config['AUTH'].get('AppId') == "REDACTED":
+    raise Exception("Configure your application ID in 'config.ini', try again.")
 else:
-    command_prefix = None
+    app_id = config['INFO'].get('AppId')
+    bot = commands.Bot(command_prefix=command_prefix,
+        intents=discord.Intents.all(), 
+        application_id=app_id)
 
-bot = commands.Bot(command_prefix=command_prefix,
-    intents=discord.Intents.all(),
-    case_insensitive=True)
-
-# mypy doesnt understand an implicit setattr
+# mypy doesn't understand an implicit setattr
 setattr(bot, 'config', config)
 
 cogs = [
-    # 'afk',
-    # 'auditlog',
-    # 'automod',
-    # 'bankofdiscord',
-    # 'boostreward',
-    # 'bulletin',
-    # 'captcha',
-    # 'courier',
-    # 'crosspost',
-    # 'customcmd',
-    # 'discorddefender',
-    # 'embed',
-    # 'emoji',
-    # 'form',
-    # 'fun',
-    # 'giveaway',
     'utility.help',
-    # 'information',
-    # 'invitemonitor',
-    # 'joinleave',
-    # 'kingofword',
-    # 'level',
-    # 'moderation',
-    # 'music',
-    # 'profile',
-    # 'reactionrole',
-    # 'roomie',
-    # 'serverstat',
-    # 'utility',
-    # 'vault',
-    # 'voicemail',
-    ]
+    'moderation.moderation',
+]
 
-name = config['INFO'].get('BotName') + ":"
+botname = config['INFO'].get('BotName')
 async def load_all_cogs():
     for extension in cogs:
         try:
             await bot.load_extension("cogs." + extension)
-            print(f'{name} Loaded {extension} cog.')
+            print(f'{botname}: Loaded {extension} cog.')
         except Exception as error:
-            print(f'{name} Error loading {extension} cog: {error}')
-asyncio.run(load_all_cogs())
+            print(f'{botname}: Error loading {extension} cog: {error}')
+    await bot.tree.sync()
 
 @bot.event
 async def on_ready() -> None:
-    # Setting 'Watching' status
+    # set watching status
     await bot.change_presence(activity=discord.Activity(
-        type=discord.ActivityType.watching, name='for @Wyvern'))
-    print(f"{name} Successfully connected to Discord.\n" + "-" * 37)
+        type=discord.ActivityType.watching, name=f'for @{botname}'))
+    print(f"{botname}: Successfully connected to Discord.\n" + "-" * 37)
+    await load_all_cogs()
 
 async def main():
-    try:
-        await bot.start(config['AUTH']['BotToken'])
-    except KeyboardInterrupt:
-        print(f"{name} Keyboard interrupted, disconnecting.")
-    except Exception as error:
-        print(f"{name} Error while running: '{error}'")
-    finally:
-        async with bot:
-            await bot.logout()
+    if config['AUTH'].get('BotToken') == "REDACTED":
+        raise Exception("Configure your token in 'config.ini', try again.")
+    else:
+        try:
+            await bot.start(config['AUTH']['BotToken'])
+        except KeyboardInterrupt:
+            print(f"{botname}: Keyboard interrupted, disconnecting.")
+        except Exception as error:
+            print(f"{botname}: Error while running: '{error}'")
+        finally:
+            async with bot:
+                await bot.logout()
 
-if config['AUTH'].get('BotToken') == "REDACTED":
-    raise Exception(f"{name} Configure your token in 'config.ini', try again.")
-else:
-    asyncio.run(main())
+asyncio.run(main())
